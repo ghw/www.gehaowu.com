07610b7844c90fe3cfffa4fec0e74c876b6c18d1
Haowu Ge <ghw@bitmoe.com>
2018-07-09 04:53:28 +0800

    add

---
+++ js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = '//hm.baidu.com/hm.js?2f0f4b0d867163a00886350ad2c21f8c';
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
